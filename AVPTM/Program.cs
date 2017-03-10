ee89cf0f9e6a191c07c768581370f30ff4286edd
Tom Diethe <tdiethe@gmail.com>
2017-03-10 22:39:16 +0000

    Added initial solution with LDA model and author topic model (possibly incorrect).

---
+++ AVPTM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AVPTM
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
